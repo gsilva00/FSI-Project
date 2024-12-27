95e490db3b7cda1d9163eb4bee5ccb006eb39b44
Guilherme Silva <gmpas4444@gmail.com>
2024-12-27 16:59:40 +0000

    millerRabin DONE

--- files/CTF12/gen_example.py
+++ files/CTF12/gen_example.py
@@ -7,23 +7,55 @@ t = 12 # Número da turma
 g = 2  # Número do grupo
 
 
+# Returns True if n is probably prime, False if it's composite
+def millerRabin(n, k=5):
+    # Input #2: k, the number of rounds of testing to perform
+    # Input #1: n > 2, an odd integer to be tested for primality
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    s = 0
+    d = n - 1
+    while d % 2 == 0:
+        s += 1
+        d //= 2
+
+    # Witness loop
+    for _ in range(k):
+        a = random.randint(2, n-2)
+        x = pow(a, d, n)
+        if x == 1 or x == n-1:
+            continue
+        for _ in range(s-1):
+            y = pow(x, 2, n)
+            if y == 1 and x != 1 and x != n-1:
+                return False
+            x = y
+        if x != 1:
+            return False
+    return True
+
+def getRandomPrime(exp):
+    candidate = 2**exp
+    while not millerRabin(candidate):
+        candidate += 1
+    return candidate
+
+
 def extendedEuclidean(a, b):
     print(f"Extended Euclidean Algorithm for {a} and {b}")
     old_r, r = a, b
     old_s, s = 1, 0
     old_t, t = 0, 1
     while r != 0:
-        print("------------------------------------------------")
-        print(f"Iteration: {old_r}, {r}")
-        print(f">> old_r: {old_r}, old_s: {old_s}, old_t: {old_t}")
-        print(f">> r: {r}, s: {s}, t: {t}")
         q = old_r // r
-        print(f">> Quotient: {q}")
         old_r, r = r, old_r - q*r
         old_s, s = s, old_s - q*s
         old_t, t = t, old_t - q*t
-        print(f">> old_r: {old_r}, old_s: {old_s}, old_t: {old_t}")
-        print(f">> r: {r}, s: {s}, t: {t}")
 
     # Ensure gcd is positive
     if old_r < 0:
@@ -31,13 +63,6 @@ def extendedEuclidean(a, b):
         old_s = -old_s
         old_t = -old_t
 
-    print()
-    print(f"Bézout coefficients: {old_s}, {old_t}. That is: x,y such that a*x + b*y == g == gcd(a,b)")
-    print(f"Greatest common divisor of {a} and {b}: {old_r}")
-    print(f"Quotients: diving {a} by gcd == {t}; dividing {b} by gcd == {s}")
-    print(f"Equation: {a} * {old_s} + {b} * {old_t} == {old_r}")
-    print()
-    print(f"The returns are: {old_r}, {old_s}, {old_t}")
     return old_r, old_s, old_t
 
 def modInv(a, b):
@@ -46,7 +71,6 @@ def modInv(a, b):
     if g != 1:
         print("ERROR!! gcd(e, phi) != 1")
 
-    print(f"Modular inverse of {a} mod {b} is {x % b}")
     return x % b # returns d such that (d * e) % v == 1
 
 
